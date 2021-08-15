79fdef6ad2d29736f0b5624ef3db43f70d8ef5de
Kim-Tri Huynh <huynhkimtri.fuhcm@gmail.com>
2021-08-16 00:07:09 +0700

    feat: react router, first functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/add-project" component={AddProject} />
+      </div>
+    </Router>
   );
 }
 
